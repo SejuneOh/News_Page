0a011fba43dfa47b54dee69acb333aea1ff923fc
SejuneOh <etry0715@gmail.com>
2022-08-17 20:57:31 +0900

    feat: Develop Header component

--- newsClient/src/App.tsx
+++ newsClient/src/App.tsx
@@ -1,14 +1,17 @@
-import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import SearchPage from "./routes/SearchPage";
 import ClipsPage from "./routes/ClipsPage";
+import Header from "./componets/Header";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<SearchPage />}></Route>
-      <Route path="/clips" element={<ClipsPage />}></Route>
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        {/* <Route path="/" element={<SearchPage />}></Route> */}
+        {/* <Route path="/clips" element={<ClipsPage />}></Route> */}
+      </Routes>
+    </>
   );
 }
 
---
+++ newsClient/src/componets/Header.tsx
@@ -0,0 +1,76 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const HeaderStyle = styled.div`
+  border: 1px solid gray;
+  position: fixed;
+  width: 100%;
+`;
+
+const Title = styled.div`
+  text-align: center;
+  border-bottom: 1px solid #03cf5b;
+  padding: 10px 0;
+  .title {
+    font-size: 40px;
+    font-weight: 800;
+    color: #03cf5b;
+  }
+`;
+
+const Taps = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  text-align: center;
+`;
+
+const Option = styled(Link)`
+  display: block;
+  font-size: 25px;
+  text-decoration: none;
+  width: 100%;
+  height: 50px;
+  font-weight: 500;
+  border: 1px solid #dae1e6;
+  line-height: 50px;
+  cursor: pointer;
+
+  &.active {
+    color: white;
+    background-color: #65c971;
+  }
+
+  &.disabled {
+    background-color: #fff;
+    color: black;
+  }
+`;
+
+export default function Header() {
+  const [active, setActive] = useState<boolean>(true);
+
+  return (
+    <HeaderStyle>
+      <Title>
+        <span className="title">NEVER NEWS</span>
+      </Title>
+      <Taps>
+        <Option
+          className={active ? "active" : "disabled"}
+          to="/"
+          onClick={() => setActive((prev) => !prev)}
+        >
+          SEARCH
+        </Option>
+        <Option
+          className={active ? "disabled" : "active"}
+          to="/clips"
+          onClick={() => setActive((prev) => !prev)}
+        >
+          CLIPS
+        </Option>
+      </Taps>
+    </HeaderStyle>
+  );
+}
